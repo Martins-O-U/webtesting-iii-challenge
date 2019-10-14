d161a435ae8bb333fe20691fc129dea38ec38abc
Martins-O-U <martinsonyedikachi@gmail.com>
2019-10-14 15:03:36 +0100

    Dependencies added, library testedwith a check code

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,19 @@
-// Test away
+import React from 'react';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Dashboard from './Dashboard';
+
+
+afterEach(rtl.cleanup);
+
+let wrapper, incSyncButton;
+
+beforeEach(() => {
+  wrapper = rtl.render(<Dashboard />);
+});
+
+describe ('Dasboard component', ()=>{
+    it('Checks for the presence of the text "To test"', () => {
+        expect(wrapper.queryByText(/To test/i)).toBeInTheDocument();
+    });
+})
